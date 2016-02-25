87d29d79261442f95bfcd84bf80d9466a853ebdb
Aaron <wikea1@gmail.com>
2016-02-25 10:02:52 -0800

    Add PhotosVC class

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Aaron Williamson on 2/25/16.
+//  Copyright © 2016 Aaron Williamson. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+}
