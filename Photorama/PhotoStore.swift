5813487ffaa3269418e4a33da6e7e249a7caaa20
Aaron <wikea1@gmail.com>
2016-02-26 11:48:47 -0800

    Refactor data task to parse JSON

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -22,20 +22,16 @@ class PhotoStore {
         let request = NSURLRequest(URL: url)
         
         let task = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
-            if let jsonData = data {
-                do {
-                    let jsonObject: AnyObject = try NSJSONSerialization.JSONObjectWithData(jsonData, options: [])
-                    print(jsonObject)
-                } catch let error {
-                    print("Error creating JSON object: \(error)")
-                }
-            } else if let requestError = error {
-                print("Error fetching recent photos: \(requestError)")
-            } else {
-                print("Unexpected error with the request")
-            }
+            let result = self.processRecentPhotosRequest(data: data, error: error)
         }
         task.resume()
+    }
+    
+    func processRecentPhotosRequest(data data: NSData?, error: NSError?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .Failure(error!)
+        }
+        return FlickrAPI.photosFromJSONData(jsonData)
     }
     
 }
