08902fd8e090dd2decb34c7f2e902abb5a88d79b
Aaron <wikea1@gmail.com>
2016-02-25 23:28:22 -0800

    Test fetch and print to console from Flickr API

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -12,4 +12,12 @@ class PhotosViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
     
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchRecentPhotos()
+    }
+    
 }
